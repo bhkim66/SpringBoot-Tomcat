cd8c39e732e123d2b73888c7bd115fb1eb9491fe
bhkim66 <bhmklnj63@gmail.com>
2024-11-04 17:34:42 +0900

    외부 설정 사용

---
+++ external-read/src/main/java/hello/datasource/MyDataSourcePropertiesV1.java
@@ -0,0 +1,29 @@
+package hello.datasource;
+
+import lombok.Data;
+import lombok.Getter;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+import java.time.Duration;
+import java.util.ArrayList;
+import java.util.List;
+
+@Slf4j
+@Getter
+@ConfigurationProperties("my.datasource")
+public class MyDataSourcePropertiesV1 {
+
+    private String url;
+    private String username;
+    private String password;
+    private Etc etc;
+
+    @Data
+    public static class Etc {
+        private int maxConnection;
+        private Duration timeout;
+        private List<String> options = new ArrayList<>();
+
+    }
+}
